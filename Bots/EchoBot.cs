3fe85a4b35acb0f7a273403b884deaa0bbd1c81a
novaesf <fe022@t-systems.com.br>
2021-07-27 20:20:06 -0300

    vocedisse

--- Bots/EchoBot.cs
+++ Bots/EchoBot.cs
@@ -15,7 +15,7 @@ namespace EchoBotCli.Bots
     {
         protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
         {
-            var replyText = $"Echo: {turnContext.Activity.Text}";
+            var replyText = $"Você disse: {turnContext.Activity.Text}";
             await turnContext.SendActivityAsync(MessageFactory.Text(replyText, replyText), cancellationToken);
         }
 
